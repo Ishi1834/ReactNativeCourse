11a2b3e81543cc0e42bb62b8009fb99da48c0e92
IMohamad <ismail.mohamad@capgemini.com>
2022-07-26 17:03:35 +0100

    updated meals

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,9 +1,77 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  Image,
+  StyleSheet,
+  Platform,
+} from "react-native";
 
-export default function MealItem({ title }) {
+export default function MealItem({
+  title,
+  imageUrl,
+  duration,
+  complexity,
+  affordability,
+}) {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+      >
+        <View>
+          <Image
+            source={{
+              uri: imageUrl,
+            }}
+            style={styles.image}
+          />
+          <Text style={styles.title}>{title}</Text>
+        </View>
+        <View style={styles.details}>
+          <Text style={styles.detailItem}>{duration} m</Text>
+          <Text style={styles.detailItem}>{complexity}</Text>
+          <Text style={styles.detailItem}>{affordability}</Text>
+        </View>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+    elevation: 4,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 8,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    padding: 8,
+  },
+  details: {
+    flexDirection: "row",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    fontSize: 12,
+    marginHorizontal: 5,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+});
