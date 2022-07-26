df272b1473aa1a73843c73000ed4c8d56a4cdc34
IMohamad <ismail.mohamad@capgemini.com>
2022-07-26 12:47:28 +0100

    added category grid screen

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,55 @@
+import { View, Text, Pressable, StyleSheet, Platform } from "react-native";
+
+export default function CategoryGridTile({ title, color }) {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        android_ripple={{ color: "ccc" }}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white",
+    overflow: Platform.OS === "android" ? "hidden" : "visible", // hides ripple overflow: ;
+    /*
+     * Need backgroundColor in order for shadow to work with ios
+     * Shadow properties are for iso
+     */
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 8,
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
