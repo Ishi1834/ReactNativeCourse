2fd0edcd054bf058116940992bf4aab0824dbde8
IMohamad <ismail.mohamad@capgemini.com>
2022-07-27 17:51:07 +0100

    added styling for the list item component

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,4 +1,5 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/style";
 /**
  * object not valid as react children
  * therefore transforming it into a string
@@ -6,17 +7,52 @@ import { View, Text, Pressable, StyleSheet } from "react-native";
 export default function ExpenseItem({ description, amount, date }) {
   return (
     <Pressable>
-      <View>
-        <Text>{description}</Text>
-        <Text>{date.toString()}</Text>
-      </View>
-      <View>
-        <Text>{amount}</Text>
+      <View style={styles.expenseItem}>
+        <View>
+          <Text style={[styles.textBase, styles.description]}>
+            {description}
+          </Text>
+          <Text style={styles.textBase}>{date.toString()}</Text>
+        </View>
+        <View style={styles.amountContainer}>
+          <Text style={[styles.textBase, styles.amount]}>{amount}</Text>
+        </View>
       </View>
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
-  expenseItem: {},
+  expenseItem: {
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius: 6,
+    elevation: 3,
+    shadowColor: GlobalStyles.colors.gray500,
+    shadowRadius: 4,
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.4,
+  },
+  textBase: {
+    color: GlobalStyles.colors.primary50,
+  },
+  description: {
+    fontSize: 12,
+    marginBottom: 4,
+    fontWeight: "bold",
+  },
+  amountContainer: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+  },
+  amount: {
+    color: GlobalStyles.colors.primary500,
+  },
 });
