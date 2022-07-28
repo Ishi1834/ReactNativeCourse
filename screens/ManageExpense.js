e9bc1b4c37ac695ba91f146f053767ab275f2fe4
IMohamad <ismail.mohamad@capgemini.com>
2022-07-28 13:50:53 +0100

    connected add/edit/delete to store

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Button from "../components/UI/Button";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/style";
+import { ExpensesContext } from "../store/expenses-context";
 
 export default function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId; // only drill into expenseId if params is defined
   const isEditing = !!editedExpenseId; // convert to  boolean
+  const expenseContext = useContext(ExpensesContext);
 
   useEffect(() => {
     navigation.setOptions({
@@ -14,8 +16,26 @@ export default function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  function deleteExpenseHandler() {}
-  function handleConfirm() {}
+  function deleteExpenseHandler() {
+    expenseContext.deleteExpense(editedExpenseId);
+    navigation.goBack();
+  }
+  function handleConfirm() {
+    if (isEditing) {
+      expenseContext.updateExpense(editedExpenseId, {
+        description: "test update",
+        amount: 19.99,
+        date: new Date(),
+      });
+    } else {
+      expenseContext.addExpense({
+        description: "test",
+        amount: 19.99,
+        date: new Date(),
+      });
+    }
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
