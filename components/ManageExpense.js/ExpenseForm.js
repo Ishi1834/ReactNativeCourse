62a5240c76496e9c538e38ffe5d7ef5bd25f5149
IMohamad <ismail.mohamad@capgemini.com>
2022-07-28 17:36:33 +0100

    added validation

--- components/ManageExpense.js/ExpenseForm.js
+++ components/ManageExpense.js/ExpenseForm.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import Input from "./Input";
 import Button from "../UI/Button";
 
@@ -25,6 +25,15 @@ export default function ExpenseForm({
       date: new Date(inputValues.date),
       description: inputValues.description,
     };
+
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== "Invalid Date";
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      Alert.alert("Invalid input, plase check your input");
+      return;
+    }
     onSubmit(expenseData);
   }
 
