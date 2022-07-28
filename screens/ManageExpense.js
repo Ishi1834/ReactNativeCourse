25b41cda434371831ecd2e658b2b2c1bc31754b5
IMohamad <ismail.mohamad@capgemini.com>
2022-07-28 10:49:11 +0100

    added header selection based on expense type

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import { View, Text } from "react-native";
 
-export default function ManageExpense() {
+export default function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId; // only drill into expenseId if params is defined
+  const isEditing = !!editedExpenseId; // convert to  boolean
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+  if (isEditing) {
+    return <Text>Edit expense</Text>;
+  }
   return (
     <View>
-      <Text>ManageExpenses</Text>
+      <Text>Add expense</Text>
     </View>
   );
 }
