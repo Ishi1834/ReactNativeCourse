ca4e464de81736922f564e4caa8e9140081a6bed
IMohamad <ismail.mohamad@capgemini.com>
2022-07-28 13:22:24 +0100

    added recent expense function

--- util/date.js
+++ util/date.js
@@ -1,3 +1,16 @@
 export function getFormattedDate(date) {
   return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 }
+
+export function getDateMinusDays(date, days) {
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days);
+}
+export function getRecentExpenses(expenses) {
+  const recentExpenses = expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date > date7DaysAgo;
+  });
+  return recentExpenses;
+}
