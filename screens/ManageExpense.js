12579841502c31a9c555c08c84c2f1349dd410d1
IMohamad <ismail.mohamad@capgemini.com>
2022-07-29 17:17:35 +0100

    added loading overlay for manage page

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,12 +1,14 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import ExpenseForm from "../components/ManageExpense.js/ExpenseForm";
 import IconButton from "../components/UI/IconButton";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { GlobalStyles } from "../constants/style";
 import { ExpensesContext } from "../store/expenses-context";
 import { deleteExpense, postExpense, putExpense } from "../util/http";
 
 export default function ManageExpense({ route, navigation }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const editedExpenseId = route.params?.expenseId; // only drill into expenseId if params is defined
   const isEditing = !!editedExpenseId; // convert to  boolean
   const expenseContext = useContext(ExpensesContext);
@@ -22,12 +24,14 @@ export default function ManageExpense({ route, navigation }) {
   }, [navigation, isEditing]);
 
   async function deleteExpenseHandler() {
+    setIsSubmitting(true);
     await deleteExpense(editedExpenseId);
     expenseContext.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
 
   async function handleSubmit(expenseData) {
+    setIsSubmitting(true);
     if (isEditing) {
       await putExpense(editedExpenseId, expenseData);
       expenseContext.updateExpense(editedExpenseId, expenseData);
@@ -41,7 +45,9 @@ export default function ManageExpense({ route, navigation }) {
   function handleCancel() {
     navigation.goBack();
   }
-
+  if (isSubmitting) {
+    return <LoadingOverlay />;
+  }
   return (
     <View style={styles.container}>
       <ExpenseForm
