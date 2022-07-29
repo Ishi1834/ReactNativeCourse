b159d0c46a802214e1460fbabc70af63b0a9ea63
IMohamad <ismail.mohamad@capgemini.com>
2022-07-29 17:40:30 +0100

    added error overlay to handle broken network requests

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import ErrorOverlay from "../components/UI/ErrorOverlay";
 import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { ExpensesContext } from "../store/expenses-context";
 import { getRecentExpenses } from "../util/date";
@@ -7,18 +8,27 @@ import { getExpenses } from "../util/http";
 
 export default function RecentExpenses() {
   const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState();
   const expensesContext = useContext(ExpensesContext);
 
   useEffect(() => {
     async function fetchExpenses() {
       setIsFetching(true);
-      const expenses = await getExpenses();
+      try {
+        const expenses = await getExpenses();
+        expensesContext.setExpenses(expenses);
+      } catch (error) {
+        setError("Could not fetch expenses!");
+      }
       setIsFetching(false);
-      expensesContext.setExpenses(expenses);
     }
     fetchExpenses();
   }, []);
   const recentExpenses = getRecentExpenses(expensesContext.expenses);
+
+  if (error && !isFetching) {
+    return <ErrorOverlay message={error} onConfirm={() => setError(null)} />;
+  }
   if (isFetching) {
     return <LoadingOverlay />;
   }
