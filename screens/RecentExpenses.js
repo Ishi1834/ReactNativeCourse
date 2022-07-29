a643dbe503ecddf19d442c87317937a9026af334
IMohamad <ismail.mohamad@capgemini.com>
2022-07-29 17:11:52 +0100

    added loading spinner

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,20 +1,27 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { ExpensesContext } from "../store/expenses-context";
 import { getRecentExpenses } from "../util/date";
 import { getExpenses } from "../util/http";
 
 export default function RecentExpenses() {
+  const [isFetching, setIsFetching] = useState(true);
   const expensesContext = useContext(ExpensesContext);
 
   useEffect(() => {
     async function fetchExpenses() {
+      setIsFetching(true);
       const expenses = await getExpenses();
+      setIsFetching(false);
       expensesContext.setExpenses(expenses);
     }
     fetchExpenses();
   }, []);
   const recentExpenses = getRecentExpenses(expensesContext.expenses);
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
   return (
     <ExpensesOutput
       expenses={recentExpenses}
