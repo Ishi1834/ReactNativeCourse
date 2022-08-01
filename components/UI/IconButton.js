9c4caca6b7b3cd9ded4a8fad6a23392dfce214c1
IMohamad <ismail.mohamad@capgemini.com>
2022-08-01 14:59:39 +0100

    added icon

---
+++ components/UI/IconButton.js
@@ -0,0 +1,25 @@
+import { Pressable, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function IconButton({ onPress, icon, size, color }) {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+    >
+      <Ionicons name={icon} size={size} color={color} />
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    margin: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+});
