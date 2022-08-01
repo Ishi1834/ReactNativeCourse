f253b074b6a42bd33f054c3c03d0df1977a55277
IMohamad <ismail.mohamad@capgemini.com>
2022-08-01 11:56:38 +0100

    added token to request

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,25 @@
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
 import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const authContext = useContext(AuthContext);
+  const [fetchedmessage, setFetchedMessage] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        `https://rn-auth-example-app-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=${authContext.token}`
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  });
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedmessage}</Text>
     </View>
   );
 }
