aabe88f6cf42829d525e4f4b21c4efc2013460df
IMohamad <ismail.mohamad@capgemini.com>
2022-08-01 11:04:55 +0100

    added return to functions

--- util/auth.js
+++ util/auth.js
@@ -2,13 +2,24 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyAWnnoEwnFgQSe0dyEQnjvO1GS7s7Ng_SE";
 
-export async function createUser(email, password) {
+async function authenticate(mode, email, password) {
   const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+    `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`,
     {
       email: email,
       password: password,
       returnSecureToken: true,
     }
   );
+  return response;
+}
+
+export async function createUser(email, password) {
+  const response = await authenticate("signUp", email, password);
+  return response;
+}
+
+export async function loginUser(email, password) {
+  const response = await authenticate("signInWithPassword", email, password);
+  return response;
 }
