f306bcc1be392cf5e4a220cff52f895c42747240
IMohamad <ismail.mohamad@capgemini.com>
2022-08-02 12:37:09 +0100

    save icon to header

--- screens/Map.js
+++ screens/Map.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
-import { StyleSheet } from "react-native";
+import { useCallback, useLayoutEffect, useState } from "react";
+import { Alert, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import IconButton from "../components/UI/IconButton";
 
-export default function Map() {
+export default function Map({ navigation }) {
   const [selectedLocation, setSelectedLocation] = useState(null);
   const region = {
     latitude: 37.78,
@@ -15,6 +16,37 @@ export default function Map() {
     const latitude = event.nativeEvent.coordinate.latitude;
     setSelectedLocation({ latitude: latitude, longitude: longitude });
   }
+  /**
+   * useCallBack Hook only runs when one of its dependencies updates
+   */
+  const savePickedlocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert(
+        "No location picked!",
+        "You have to pick a location (by taping on the map)!"
+      );
+      return;
+    }
+
+    navigation.navigate("AddPlace", {
+      pickedLat: selectedLocation.latitude,
+      pickedLng: selectedLocation.longitude,
+    });
+  }, [navigation, selectedLocation]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon="save"
+          size={24}
+          color={tintColor}
+          onPress={savePickedlocationHandler}
+        />
+      ),
+    });
+  }, [navigation, savePickedlocationHandler]);
+
   return (
     <MapView
       onPress={selectLocationHandler}
