a98b4ca148209feb182234f0ea9d7d21ae88a94e
IMohamad <ismail.mohamad@capgemini.com>
2022-08-03 12:45:26 +0100

    initialzed db on app start

--- App.js
+++ App.js
@@ -6,10 +6,28 @@ import IconButton from "./components/UI/IconButton";
 import AddPlace from "./screens/AddPlace";
 import AllPlaces from "./screens/AllPlaces";
 import Map from "./screens/Map";
+import { useEffect, useState } from "react";
+import { init } from "./util/database";
+import AppLoading from "expo-app-loading";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+
+  useEffect(() => {
+    init()
+      .then(() => {
+        setDbInitialized(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (!dbInitialized) {
+    return <AppLoading />;
+  }
   return (
     <>
       <StatusBar style="auto" />
