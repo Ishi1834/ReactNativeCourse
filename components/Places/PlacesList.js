ce033bf32b14663bfedeae98a5f9dc666203f74e
IMohamad <ismail.mohamad@capgemini.com>
2022-08-03 12:20:23 +0100

    added margin to list item

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -12,6 +12,7 @@ export default function PlacesList({ places }) {
   }
   return (
     <FlatList
+      style={styles.list}
       data={places}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <PlaceItem place={item} />}
@@ -20,6 +21,9 @@ export default function PlacesList({ places }) {
 }
 
 const styles = StyleSheet.create({
+  list: {
+    margin: 20,
+  },
   fallBackContainer: {
     flex: 1,
     justifyContent: "center",
